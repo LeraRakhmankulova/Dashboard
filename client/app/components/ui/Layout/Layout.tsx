92521f3af7cabf6f64998e10b3f5eb51a087254d
LeraRakhmankulova <lerakhmankulova@mail.ru>
2023-01-06 01:17:01 +0300

    styles

--- client/app/components/ui/Layout/Layout.tsx
+++ client/app/components/ui/Layout/Layout.tsx
@@ -1,19 +1,19 @@
-import React, { FC, PropsWithChildren } from 'react'
+import React, {FC, PropsWithChildren} from 'react'
 import Meta from '@/utils/Meta'
-import { IMeta } from '@/interfaces/IMeta.interface'
+import {IMeta} from '@/interfaces/IMeta.interface'
 import Header from '../header/Header'
 
-const Layout: FC<PropsWithChildren<IMeta>> = ({ children, ...meta }) => {
-	return (
-		<>
-			<Meta {...meta}/>
-      <div>
-        <Header/>
-        <main className='p-4'>
-          {children}
-        </main>
-      </div>
-		</>
-	)
+const Layout: FC<PropsWithChildren<IMeta>> = ({children, ...meta}) => {
+    return (
+        <>
+            <Meta {...meta}/>
+            <div>
+                <Header/>
+                <main className='p-4'>
+                    {children}
+                </main>
+            </div>
+        </>
+    )
 }
 export default Layout
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
 import AuthProvider from 'providers/auth/AuthProvider'
-
+import '../styles/globals.css'
 import '../app/assets/styles/globals.sass'
 
 const queryClient = new QueryClient({
