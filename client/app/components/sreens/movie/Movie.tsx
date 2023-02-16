a739c774c2d6b30ceefeb560fc10e1e7f6bdd16f
LeraRakhmankulova <lerakhmankulova@mail.ru>
2023-02-16 14:46:27 +0300

    movie

--- client/app/components/sreens/movie/Movie.tsx
+++ client/app/components/sreens/movie/Movie.tsx
@@ -1,66 +1,62 @@
-import { useQuery } from '@tanstack/react-query'
+import {useQuery} from '@tanstack/react-query'
 import Image from 'next/image'
-import { useRouter } from 'next/router'
+import {useRouter} from 'next/router'
 
 import Layout from '@/components/ui/Layout/Layout'
 
-import { IMovie } from '@/interfaces/IMovie.interface'
+import {IMovie} from '@/interfaces/IMovie.interface'
 
-import { MovieService } from '@/services/movie.service'
+import {MovieService} from '@/services/movie.service'
 
 import ReviewForm from '../reviews/ReviewForm'
 import Reviews from '../reviews/Reviews'
 
 import style from './Movie.module.sass'
+import movieMock from '../../../assets/images/mvMock.svg';
+import mockCover from '../../../../public/card-img.png'
 
 const Movie = () => {
-	const { query } = useRouter()
-	const movieId = Number(query?.id)
-
-	const {
-		data: movie,
-		isLoading
-	} = useQuery(
-		['get movie', query?.id],
-		() => MovieService.getMovieById(movieId),
-		{
-			select: ({ data }) => data
-		}
-	)
-	return (
-		<Layout title={`${movie?.name}`}>
-			<div className={style.movie_page}>
-				<div className={style.movie_page__poster}>
-					<Image
-						src={movie?.poster || ''}
-						alt={movie?.name}
-						width={290}
-						height={415}
-					/>
-				</div>
-				<div className={style.movie_page__info}>
-					<h1>{movie?.name}</h1>
-					<h3>{movie?.rating}</h3>
-					<h2>About movie:</h2>
-					<ul>
-						<li>
-							<span>Fees: </span>
-							<span>$ {movie?.fees}</span>
-						</li>
-						<li>
-							<span>Views: </span>
-							<span>{movie?.views}</span>
-						</li>
-					</ul>
-				</div>
-			</div>
-			<Reviews
-				movieId={movieId}
-				reviews={movie?.reviews || []}
-				isLoading={isLoading}
-			/>
-		</Layout>
-	)
+    const movie = {
+        movieId: 1,
+        name: "Wonderful Woman",
+        rating: 123,
+        fees: 3456789,
+        views: 456789,
+        reviews: 454689
+    }
+    return (
+        <Layout title={`${movie?.name}`}>
+            <div className={style.movie}>
+                <div className={style.movie__page}>
+                    <h1>{movie?.name}</h1>
+                    <div className={style.movie__info}>
+                        <Image src={mockCover}/>
+                        <div className={style.movie__block}>
+                            <h3>{movie?.rating}</h3>
+                            <h2>About movie:</h2>
+                            <ul>
+                                <li>
+                                    <span>Fees: </span>
+                                    <span>$ {movie?.fees}</span>
+                                </li>
+                                <li>
+                                    <span>Views: </span>
+                                    <span>{movie?.views}</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className={style.movie__video}>
+                        <Image src={movieMock}/>
+                    </div>
+                </div>
+                {/*<Reviews*/}
+                {/*    movieId={movieId}*/}
+                {/*    reviews={movie?.reviews || []}*/}
+                {/*/>*/}
+            </div>
+        </Layout>
+    )
 }
 
 export default Movie
