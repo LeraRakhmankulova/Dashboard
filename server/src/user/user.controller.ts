a0175df154ba65bdcfa82131e6abe3c176990243
LeraRakhmankulova <lerakhmankulova@mail.ru>
2023-02-28 11:40:38 +0300

    Update user.controller.ts

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {Controller, Get, Post, Body, Patch, Param, Delete, UseGuards} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import {AuthGuard} from "@nestjs/passport";
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService,) {}
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
@@ -17,11 +18,13 @@ export class UserController {
     return this.userService.findAll();
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOneById(+id);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
